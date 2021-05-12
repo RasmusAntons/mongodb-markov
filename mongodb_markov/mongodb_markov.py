46f87dd96547c1c3cf5bb79527e6384470ab4e2d
mail@rasmusantons.de <mail@rasmusantons.de>
2021-05-12 02:31:24 +0200

    fix matching consecutive delimiters

--- mongodb_markov/mongodb_markov.py
+++ mongodb_markov/mongodb_markov.py
@@ -17,7 +17,7 @@ class Triple:
 
 
 class MongodbMarkov:
-    delimiters = r'([ !"#$%&\'()*+,-./:;<=>?\[\\\]^_`{|}~])'
+    delimiters = r'([ !"#$%&\'()*+,-./:;<=>?\[\\\]^_`{|}~]+)'
 
     def __init__(self, db_client=None, db_url='localhost:27017', db_name='mongodb_markov'):
         self.db_url = db_url
